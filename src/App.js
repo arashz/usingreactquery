9081957b521aebf892d6c22b6d79002acd28a88a
Arash Zolfaghari <arashz@gmail.com>
2021-05-31 12:13:19 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-function App() {
+function Example() {
+  const getStuff = () =>
+    fetch('https://jsonplaceholder.typicode.com/users').then((res) =>
+      res.json(),
+    );
+
+  const { isLoading, error, data, isFetching, status } = useQuery(
+    'stuff',
+    getStuff,
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>An error has occurred: {error.message}</div>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      {status === 'success' &&
+        !error &&
+        data.map((d) => {
+          return (
+            <div style={{ padding: 5 }}>
+              {d.name} : {d.email}
+            </div>
+          );
+        })}
     </div>
   );
 }
 
-export default App;
+//export default App;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Example />
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
+}
